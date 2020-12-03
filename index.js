e6d59cf9491fd638cf77b0a0d8913313e6f85241
Nikozs <niko.purola@gmail.com>
2020-12-03 22:01:47 +0200

    removed useless stuff

--- index.js
+++ index.js
@@ -16,35 +16,13 @@ app.use(express.static("public"));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
-
 app.use("/auth", authRoutes);
 app.use("/blogs", blogs);
 app.use("/user", passport.authenticate("jwt", { session: false }), userit);
 
-
-// app.use("/login", express.static("public/login.html"))
-// app.use("/register", express.static("public/register.html"))
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
 app.post("/login", passport.authenticate("jwt"), function (req, res) {
   res.send(req.user);
 });
-
-/*app.get("/login", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/login.html"));
-});*/
-
-/*app.get("/register", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/register.html"));
-});
-
-app.get("/index", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-*/
 
 app.get("/blog", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/blog.html"));
