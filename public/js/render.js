db79d334d5f595d36c7c4328413361975076f77d
Nikozs <niko.purola@gmail.com>
2020-12-07 19:27:55 +0200

    check if user owns the blog and more popups

--- public/js/render.js
+++ public/js/render.js
@@ -38,7 +38,6 @@ const renderSelectedPost = async () => {
     getBlogById(blogid);
 
     // Lets print in innerhtml here...
-
   }
 };
 
@@ -47,24 +46,28 @@ const renderUsersBlogs = async () => {
   const queryString = window.location.search;
   console.log(queryString);
 
-  mainContainer.innerHTML =     
-  `<div class="header">
-  <h2>Blog Name</h2>
-  <button id="editBlogInfo">Edit</button>
-  </div>`
-  
   const urlParams = new URLSearchParams(queryString);
   if (urlParams.has("ShowUserid")) {
-    const blogid = urlParams.get("ShowUserid");
-    console.log(blogid);
-    var userinblogit = await getBlogsByUserId(blogid);
+    const userid = urlParams.get("ShowUserid");
+    console.log(userid);
+    var userinblogit = await getBlogsByUserId(userid);
 
-    var leftcolumn= `<div class="leftcolumn">
-    <button id="addNewPost" onclick="openNewBlogForm()" style="width: 100%">Add new</button>`
+   
+      mainContainer.innerHTML = `<div class="header">`
+      const header = document.querySelector(".header");
+         if (sessionStorage.getItem("loggedUserId") !== undefined && userid == sessionStorage.getItem("loggedUserId")) {
+          header.innerHTML += `<button id="editBlogInfo" onclick="editBlogInfoForm()">Edit</button>`
+      }
+      header.innerHTML +=`<h2>Blog Name</h2></div></div>`
+
+    var leftcolumn = `<div class="leftcolumn">`
+
+    if (sessionStorage.getItem("loggedUserId") !== undefined && userid == sessionStorage.getItem("loggedUserId")) {
+    leftcolumn+=`<button id="addNewPost" onclick="openNewBlogForm()" style="width: 100%">Add new</button>`
+    }
 
     for (var b = 0; b < userinblogit.length; b++) {
       var blogi = userinblogit[b];
-
       leftcolumn +=  
       `<div class="card">
       <h3> ` + blogi.Title + `</h3>
@@ -74,8 +77,13 @@ const renderUsersBlogs = async () => {
       <p class="content">Sisältö: ` + blogi.Content + `</p>
       </div>
       <p class="blogLikes">Likes: ` + blogi.amountOfLikes + ` <button>+</button><button>-</button></p>
-      <p class="blogCategory">Category: <button>dfg</button></p>
-      </div>`
+      <p class="blogCategory">Category: <button>dfg</button></p>`
+
+      if (sessionStorage.getItem("loggedUserId") !== undefined && blogi.UserID == sessionStorage.getItem("loggedUserId")) {
+        leftcolumn+='<button onclick="openModifyBlogForm()">Muokkaa</button>';
+      }
+
+      leftcolumn+=`</div>`
     }
 
     leftcolumn+= `</div>`
@@ -87,7 +95,6 @@ const renderUsersBlogs = async () => {
           <h2>About Me</h2>
           <div class="fakeimg" style="height: 100px">Image</div>
           <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-          <button id="editAboutMe">Edit</button>
         </div>
         <div class="card">
           <h3>Popular Post</h3>
@@ -99,6 +106,7 @@ const renderUsersBlogs = async () => {
         </div>
       </div>
     </div>`
+
   } else if (sessionStorage.getItem("loggedUserId") !== undefined) {
     const blogid = sessionStorage.getItem("loggedUserId");
     console.log(blogid);
@@ -123,8 +131,34 @@ const renderUsersBlogs = async () => {
     }
   } else {
     // Current user
-    console.log("ffsd");
+    console.log("Ei ole määriteltyä käyttäjää"); 
   }
+};
+
+
+function editBlogInfoForm() {
+  var popup = document.createElement("div");
+  popup.setAttribute("id", "popup");
+  popup.innerHTML = 
+  `<div id="editBlogInfoPopupoverlay" class="modaloverlay">
+  <div id="editBlogInfoPopup">
+  <h1>Edit your profile:</h1>
+  <form id="newBlogForm">
+  <label for="BlogName"><b>Blog Name</b></label>
+    <input type="text" placeholder="Enter Blog Name" name="BlogName" required>
+    <br>
+    <label for="AboutMe"><b>About Me:</b></label>
+    <textarea id="description" rows="10" cols="30" placeholder="Enter description" name="content" required></textarea><br>
+    
+    <p>Add profile image:</p>
+
+    <button type="submit">Confirm</button>
+    <button type="submit" onclick="closeForm()">Cancel</button>
+  </form>
+  </div>
+  `;
+  document.body.appendChild(popup);
+  document.body.style = "overflow: hidden";
 };
 
 function openNewBlogForm() {
@@ -135,26 +169,151 @@ function openNewBlogForm() {
   <div id="newBlogPopup">
   <h1>Add new blog</h1>
   <form id="newBlogForm">
-  <label for="title"><b>Title</b></label>
+  <label for="title"><b>Title: </b></label>
     <input type="text" placeholder="Enter title" name="title" required>
 
-    <label for="content"><b>Content</b></label>
-    <input type="content" placeholder="Enter content" name="content" required>
+    <p>Add image:</p>
 
+    <label for="content"><b>Blog text: </b></label>
+    <textarea id="content" rows="10" cols="30" placeholder="Enter content" name="content" required></textarea><br>
+
+    <label for="category">Choose category:</label>
+    <select name="category" id="category">
+      <option value="category1">category1</option>
+      <option value="category2">category2</option>
+      <option value="category3">category3</option>
+      <option value="category4">category4</option>
+    </select> 
+    <br>
     <button type="submit">Add</button>
-    <button type="submit" onclick="closeNewBlogForm()">Cancel</button>
+    <button type="submit" onclick="closeForm()">Cancel</button>
   </form>
   </div>
   `;
   document.body.appendChild(popup);
   document.body.style = "overflow: hidden";
-}
+};
 
-function closeNewBlogForm() {
+
+function openModifyBlogForm() {
+  var popup = document.createElement("div");
+  popup.setAttribute("id", "popup");
+  popup.innerHTML = 
+  `<div id="modifyBlogPopupoverlay" class="modaloverlay">
+  <div id="modifyBlogPopup">
+  <h1>Edit blog</h1>
+  <form id="modifyBlogForm">
+  <label for="title"><b>New title: </b></label>
+    <input type="text" placeholder="Enter title" name="title" required>
+
+    <p>Change image:</p>
+
+    <label for="content"><b>Edit Blog text: </b></label>
+    <textarea id="content" rows="10" cols="30" placeholder="Enter content" name="content" required></textarea><br>
+
+    <label for="category">Change category:</label>
+    <select name="category" id="category">
+      <option value="category1">category1</option>
+      <option value="category2">category2</option>
+      <option value="category3">category3</option>
+      <option value="category4">category4</option>
+    </select> 
+    <br>
+    <button type="submit">Add</button>
+    <button type="submit" onclick="closeForm()">Cancel</button>
+  </form>
+  </div>
+  `;
+  document.body.appendChild(popup);
+  document.body.style = "overflow: hidden";
+};
+
+function closeForm() {
   document.body.removeChild(popup);
   document.body.style = "overflow: auto";
+};
+
+
+async function renderRandomBlogs() {
+  var randomBlogit = await getRandomBlogs();
+
+  for (var b = 0; b < randomBlogit.length; b++) {
+    var blogi = randomBlogit[b];
+
+    randomBlogsit.innerHTML +=
+    `<li>
+    <img></img>
+    <h3> ` + blogi.Title + `</h3>
+    <p>Likes: ` + blogi.amountOfLikes + `</p>
+    <p>User: ` + blogi.UserID + `</p>
+    <button>View</button>
+    </li>`;
+  }
 }
 
+async function renderPopularBlogs() {
+  var popularBlogit = await getPopularBlogs();
+
+  for (var b = 0; b < popularBlogit.length; b++) {
+    var blogi = popularBlogit[b];
+
+    popularBlogsit.innerHTML +=
+      `<li>
+      <img style="width: 100%; height: 250px"></img>
+      <h3> ` + blogi.Title + `</h3>
+      <p>Likes: ` + blogi.amountOfLikes + `</p>
+      <p>User: ` + blogi.UserID + `</p>
+      <button onclick="renderSelectedPost()">View</button>
+      </li>`;
+  }
+}
+
+async function renderSearchResults(jep) {
+  var blogit = await getBlogs();
+
+  searchResults.innerHTML = "<h1>Search Results</h1>";
+
+  if (blogit.length > 0) {
+    for (var b = 0; b < blogit.length; b++) {
+      var blogi = blogit[b];
+      console.log("blogi", blogi);
+      searchResults.innerHTML += "<h3>" + blogi.Title + "</h3>";
+      searchResults.innerHTML += "<p>" + blogi.Content + "</p>";
+      searchResults.innerHTML += "<button>Go</button>";
+    }
+  } else {
+    searchResults.innerHTML += "<p>No results</p>";
+  }
+
+  searchResults.style.display = "block";
+  loginForm.style.display = "none";
+  signupForm.style.display = "none";
+  mainContainer.style.display = "none";
+}
+
+async function onPageLoading() {
+  // If we have param
+  const urlParams = new URLSearchParams(queryString);
+
+  if (urlParams.has("ShowUserid")) {
+    // Lets render current users blog
+    renderUsersBlogs();
+  } else if (urlParams.has("blogid")) {
+    // lets render only this one blog
+    renderSelectBlog();
+  } else {
+    // Lets render popular/randomblogs
+    renderPopularBlogs();
+    renderRandomBlogs();
+  }
+}
+
+
+
+
+/*
+  Communications with backend
+*/
 const getBlogById = async (blogid) => {
   try {
     const response = await fetch(url + "/blogs/" + blogid);
@@ -217,77 +376,77 @@ const getPopularBlogs = async () => {
   }
 };
 
-async function onPageLoading() {
-  // If we have param
-  const urlParams = new URLSearchParams(queryString);
-
-  if (urlParams.has("ShowUserid")) {
-    // Lets render current users blog
-    renderUsersBlogs();
-  } else if (urlParams.has("blogid")) {
-    // lets render only this one blog
-    renderSelectBlog();
-  } else {
-    // Lets render popular/randomblogs
-    renderPopularBlogs();
-    renderRandomBlogs();
+const getUsers = async () => {
+  try {
+    const options = {
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    };
+    const response = await fetch(url + "/user", options);
+    const users = await response.json();
+    createUserOptions(users);
+  } catch (e) {
+    console.log(e.message);
   }
-}
+};
 
-async function renderRandomBlogs() {
-  var randomBlogit = await getRandomBlogs();
 
-  for (var b = 0; b < randomBlogit.length; b++) {
-    var blogi = randomBlogit[b];
-
-    randomBlogsit.innerHTML +=
-    `<li>
-    <img></img>
-    <h3> ` + blogi.Title + `</h3>
-    <p>Likes: ` + blogi.amountOfLikes + `</p>
-    <p>User: ` + blogi.UserID + `</p>
-    </li>`;
+const addBlog = async (blogData) => {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+      },
+      body: blogData,
+    };
+    return await doFetch(url + '/blogs', options);
+  } catch (e) {
+    throw new Error(e.message);
   }
-}
+};
 
-async function renderPopularBlogs() {
-  var popularBlogit = await getPopularBlogs();
-
-  for (var b = 0; b < popularBlogit.length; b++) {
-    var blogi = popularBlogit[b];
-
-    popularBlogsit.innerHTML +=
-      `<li>
-      <img></img>
-      <h3> ` + blogi.Title + `</h3>
-      <p>Likes: ` + blogi.amountOfLikes + `</p>
-      <p>User: ` + blogi.UserID + `</p>
-      </li>`;
+const modifyBlog = async (data) => {
+  try {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+      },
+      body: JSON.stringify(data),
+    };
+    return await fetch(url + '/blogs', options);
+  } catch (e) {
+    throw new Error(e.message);
   }
-}
+};
 
-async function renderSearchResults(jep) {
-  var blogit = await getBlogs();
-
-  searchResults.innerHTML = "<h1>Search Results</h1>";
-
-  if (blogit.length > 0) {
-    for (var b = 0; b < blogit.length; b++) {
-      var blogi = blogit[b];
-      console.log("blogi", blogi);
-      searchResults.innerHTML += "<h3>" + blogi.Title + "</h3>";
-      searchResults.innerHTML += "<p>" + blogi.Content + "</p>";
-      searchResults.innerHTML += "<button>Go</button>";
-    }
-  } else {
-    searchResults.innerHTML += "<p>No results</p>";
+const deleteBlog = async (id) => {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+    },
+  };
+  try {
+    return await fetch(url + '/blogs/' + id, options);
+  } catch (e) {
+    // console.log(e.message());
+    throw new Error(e.message);
   }
+};
 
-  searchResults.style.display = "block";
-  loginForm.style.display = "none";
-  signupForm.style.display = "none";
-  mainContainer.style.display = "none";
-}
+
+const addLike = async (id) => {
+
+};
+
+
+const removeLike = async (id) => {
+
+};
 
 
 const createUserOptions = (users) => {
@@ -305,21 +464,10 @@ const createUserOptions = (users) => {
   });
 };
 
-const getUsers = async () => {
-  try {
-    const options = {
-      headers: {
-        Authorization: "Bearer " + sessionStorage.getItem("token"),
-      },
-    };
-    const response = await fetch(url + "/user", options);
-    const users = await response.json();
-    createUserOptions(users);
-  } catch (e) {
-    console.log(e.message);
-  }
-};
 
+/* 
+  Listeners
+*/
 signupForm.addEventListener("submit", async (evt) => {
   evt.preventDefault();
 
@@ -371,6 +519,7 @@ signupForm.addEventListener("submit", async (evt) => {
   getUsers();
   alert('Rekisteröinti onnistui!');
   }
+  window.location.reload();
 });
 
 
@@ -426,9 +575,17 @@ loginForm.addEventListener("submit", async (evt) => {
     getUsers();
 
     alert("Kirjautuminen onnistui!");
+    window.location.reload();
   }
 });
 
+navMyblog.addEventListener("click", async (evt)=>{
+  //evt.preventDefault();
+  navMyblog.setAttribute("class", "active")
+  navLogin.setAttribute("class", "");
+  navHome.setAttribute("class", "");
+  navSignup.setAttribute("class", "")
+});
 
 navLogout.addEventListener("click", async (evt) => {
   evt.preventDefault();
@@ -456,6 +613,7 @@ navLogout.addEventListener("click", async (evt) => {
   } catch (e) {
     console.log(e.message);
   }
+  window.location.reload();
 });
 
 navSignup.addEventListener("click", async (evt) => {
@@ -464,6 +622,7 @@ navSignup.addEventListener("click", async (evt) => {
   navSignup.setAttribute("class", "active");
   navLogin.setAttribute("class", "");
   navHome.setAttribute("class", "");
+  navMyblog.setAttribute("class", "")
 
   searchResults.style.display = "none";
   loginForm.style.display = "none";
@@ -478,6 +637,7 @@ navLogin.addEventListener("click", async (evt) => {
   navLogin.setAttribute("class", "active");
   navHome.setAttribute("class", "");
   navSignup.setAttribute("class", "");
+  navMyblog.setAttribute("class", "")
 
   searchResults.style.display = "none";
   frontpageContainer.style.display = "none";
@@ -492,6 +652,7 @@ navHome.addEventListener("click", async (evt) => {
   navHome.setAttribute("class", "active");
   navLogin.setAttribute("class", "");
   navSignup.setAttribute("class", "");
+  navMyblog.setAttribute("class", "");
 
   frontpageContainer.style.display = "block";
   signupForm.style.display = "none";
